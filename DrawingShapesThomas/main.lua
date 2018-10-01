70f7ee03c50a5a356384fae0abc70f3c4e7cd32e
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-10-01 13:22:12 -0400

    Added triangle

--- DrawingShapesThomas/main.lua
+++ DrawingShapesThomas/main.lua
@@ -1,7 +1,20 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: Displaying shapes in Lua
+-- Name: Thomas Wehbi
+-- Course: ICS20
+-- this program diplays shapes in Lua
 
--- Your code here
+
+local halfW = display.contentWidth * 0.5
+
+local halfH = display.contentHeight * 0.5
+ 
+local vertices = { 0,110, 65,90, 65,90 }
+
+local triangle = display.triangle( HalfW, HalfH, vertices )
+triangle.fill = { type="image", filename="mountains.png" }
+triangle.strokeWidth = 10
+
+
+
+
+
