400238e0fa9cfbd6c99f3087f70aad5d2ecec7a1
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-10-17 20:32:18 -0400

    finished animating images

--- AnimatingImagesThomasW/main.lua
+++ AnimatingImagesThomasW/main.lua
@@ -33,14 +33,37 @@ local rocket2 = display.newImageRect("Images/rocket2.png", 350, 350)
 local satellite = display.newImageRect("Images/satellite.png", 350, 400)
 
 -- set the initial x and y positions of each image
-astronaut.x = 130
-astronaut.y = display.contentHeight/1.5
+--astronaut.x = 130
+---astronaut.y = display.contentHeight/1.5
 
+astronaut.x = 100
+astronaut.y = display.contentWidth/2
 rocket2.x = 900
 rocket2.y = display.contentWidth/2
 
 satellite.x = 450
 satellite.y = display.contentHeight/3
 
+-- set this image to end transparent
+--astronaut.alpha = 0
+transition.to(astronaut,{x=270, y= 450, time=12000})
+transition.to(rocket2,{x=900, y=350, alpha=0, time=12000})
+transition.to(satellite,{xScale=.3, yScale=.4, x= 450, y=600, time=12000})
 
+-- set scroll speed 
+scrollSpeed = 3
+
+	-- Function: Moveastronaut
+	-- Input: this function accepts an event listener
+	-- Output: none
+	-- Description: this function adds the scroll speed to the x- value of the ship and it will appear
+	local function Moveastronaut(event)
+
+	astronaut.x = astronaut.y + scrollSpeed
+
+	astronaut.alpha = astronaut.alpha +0.01
+
+end
+
+-- add the scroll speed to the x- value of the image and it will appear
 
