6765f67d868d63047602c5b90289a23c9452086a
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-10-20 15:42:01 -0400

    added folders to Files

--- AnimatingImagesThomasW/main.lua
+++ AnimatingImagesThomasW/main.lua
@@ -7,21 +7,21 @@
 -- hide the status bar 
 display.setStatusBar(display.HiddenStatusBar)
 
-local backgroundImage = display.newImageRect("Images/space.jpg", 2480, 1550)
-
 -- add background with height and width
+-- Creating new variable and assigning it
 local backgroundImage = display.newImageRect("Images/space.jpg", 2480, 1550)
 
 -- add background with height and width 
-local astronaut = display.newImageRect("Images/astronaut.png", 350, 400)
+local astronaut 
+astronaut = display.newImageRect("Images/astronaut.png", 350, 400)
 
-local rocket2 = display.newImageRect("Images/rocket2.png", 350, 350)
+local rocket2 
+rocket2 = display.newImageRect("Images/rocket2.png", 350, 350)
 
-local satellite = display.newImageRect("Images/satellite.png", 350, 400)
+local satellite 
+satellite = display.newImageRect("Images/satellite.png", 350, 400)
 
 --set the initial x and y positions of each image
---astronaut.x = 130
---astronaut.y = display.contentHeight/1.5
 
 astronaut.x = 100
 astronaut.y = display.contentWidth/2
@@ -36,6 +36,9 @@ satellite.y = display.contentHeight/3
 transition.to(astronaut,{x=270, y= 450, time=12000})
 transition.to(rocket2,{x=900, y=350, alpha=0, time=12000})
 transition.to(satellite,{ x= 450, y=600, xScale=.3, yScale=.4, time=12000})
+-- transition.to(astronaut, {rotation=90})
+-- transition.to(rocket2, {height = 700, time = 5000})
+-- transition.to(satellite, {y = 850, delay = 5000, time = 6000})
 
 
 
