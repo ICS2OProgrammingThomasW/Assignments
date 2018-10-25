1456125f390632a72aea92fd79e4b62bacbfc45a
ThomasW2004 <thomas.wehbi@ocsbstudent.ca>
2018-10-25 11:29:10 -0400

    Added math quiz

--- AnimatingImagesThomasW/main.lua
+++ AnimatingImagesThomasW/main.lua
@@ -4,25 +4,35 @@
 -- Course: ICS2O
 -- This program displays 3 images that move across the screen.
 
--- hide the status bar 
+-- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
--- global variables
-scrollSpeed = 3
+local backgroundImage = display.newImageRect("Images/sapce.jpg", 2480, 1550)
 
---background image with width and height
-local backgroundImage = display.newImageRect("Images/space.jpg", 2048, 1536)
+-- add background with height and width
+local backgroundImage = display.newImageRect("Images/space.jpg", 2480, 1550)
 
--- character image with width and height
-local astronaut = display.newImageRect("Images/astronaut", 200, 200)
+-- add background with height and width
+local astronaut = display.newImageRect("Images/astronaut.png", 350, 400)
 
--- set the image to be transparent
-astronaut.alpha = 0
+local rocket2 = display.newImageRect("Images/rocket2.png", 350, 350)
 
--- set the initial x and y position of space 
-astronaut.x = 0
-astronaut.y = display.contentHeight/3
+local satellite = display.newImageRect("Images/satellite.png", 350, 400)
 
--- function: MoveAstronaut
--- Input: this function accepts an event listener
--- output: none
+--set the initial x and y positions of each image
+astronaut.x = 130
+astronaut.y = display.contentHeight/1.5
+
+astronaut.x = 100
+astronaut.y = display.contentWidth/2
+rocket2.x = 900
+rocket2.y = display.contentWidth/2
+
+satellite.x = 450
+satellite.y = display.contentHeight/3
+
+--set this image to end transparent
+--astronaut.alpha = 0
+transition.to(astronaut,{x=270, y=450, time=12000})
+transition.to(rocket2,{x=900, y=350, alpha=0, time=12000})
+transition.to(satellite,{x=450, y=600, xScale=.3, yScale=.4, time=12000})
