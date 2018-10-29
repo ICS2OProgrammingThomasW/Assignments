28302ae26ef1642555d8b1a52392d8d49c14f060
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-10-28 21:01:48 -0400

    math quiz

--- MathQuizThomasW/main.lua
+++ MathQuizThomasW/main.lua
@@ -1,7 +1,14 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: Math Quiz
+-- Name: Thomas Wehbi
+-- Course: ICS2O
+-- Program: This program displays 3 hearts, i fyou get a question wrong you loose a life, if you run out of time you loose a life.
+-- If you get three questions wrong a game over image displays. If you get 5 questions right you win the game and a you win Image
+-- displays.
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+
+-- set the background color
+display.setDefault("background", 95/255, 70/255, 215/255)
