4d16463ca413c4ea9a247c3133a3232fd1459747
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-11-19 15:23:54 -0500

    company logo

--- ComapnyLogoAnimationThomasW/main.lua
+++ ComapnyLogoAnimationThomasW/main.lua
@@ -1,41 +1,41 @@
 -----------------------------------------------------------------------------------------
 -- CompanyLogoAnimation
--- Name: Thomas Wehbi
 -- Course: ICS2O
+-- Name: Thomas Wehbi
 -- Description: This program will display my clients on the screen that will move around etc.
 
--- Hide status bar
-display.setStatusBar(display.HiddenStatusBar)
+
+display.setStatusBar(display.HiddenStatusBar)				 -- Hide status bar
 
 counter = 0
 
 -- sets the background color 
-display.setDefault("background", 242/255, 5/255, 181/255)
+display.setDefault("background", 242/255, 5/255, 181/255) -- setting the color of the background.
 
 -- Adding the Logo Image
-local logo = display.newImageRect("Images/pumpkinSpiceLogo.png", 500, 500)
+local logo = display.newImageRect("Images/pumpkinSpiceLogo.png", 500, 500) -- displaying the image on screen.
 
 -- add border to logo
-logo:setStrokeColor(0, 0, 0) 
-logo.strokeWidth = 5
+logo:setStrokeColor(0, 0, 0) -- setting the border of the Image 
+logo.strokeWidth = 5 			-- width of bordre
 
 -- Logo X and Y
-logo.x = 600
+logo.x = 600					-- setting the coordinates of the Image
 logo.y = display.contentHeight/2
 
 
 local function moveLogo()
-	logo.x = logo.x + math.random(-10,10)
+	logo.x = logo.x + math.random(-10,10)  -- making the image move the X and Y on the screen to another very quickly from numbers between -10 and 10.
 	logo.y = logo.y + math.random(-10,10)
 end
 
-local function shrinkLogo()
+local function shrinkLogo()  				-- Used from internet
 	logo.width = logo.width * 0.99
 	logo.height = logo.height * 0.99
 end
 
 local function increaseAlpha()
-	logo.alpha = logo.alpha + 0.01
+	logo.alpha = logo.alpha + 0.01 -- transparency of Image 
 end
 
 local function animateLogo() -- used some from internet
@@ -51,7 +51,7 @@ local function animateLogo() -- used some from internet
 end
 
 logo.alpha = 0
-timer.performWithDelay( 10, animateLogo, 300)
+timer.performWithDelay( 10, animateLogo, 300) -- calling the function animateLogo to do something.
 
 
 
